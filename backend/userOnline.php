744f03b0dfbe2b215cdc22a9c11a4aa4e7aba179
Kushagra Sharma <giantkillerkushagra@gmail.com>
2022-01-02 14:00:54 +0530

    db connections

--- backend/userOnline.php
+++ backend/userOnline.php
@@ -9,29 +9,4 @@ if (!$result) {
 }
 
 
-//check the time in database
-$sql = "SELECT usertime FROM leaderboard WHERE username = '" . $_SESSION['username'] . "'";
-$result = mysqli_query($conn, $sql);
-if (!$result) {
-    echo "error getting time";
-}
-
-$result = mysqli_fetch_assoc($result);
-$time = $result['usertime'];
-
-//get currentTime in IST
-$currentTime = new DateTime("now", new DateTimeZone('Asia/Kolkata'));
-$currentTime = $currentTime->format('Y-m-d H:i:s');
-
-
-
-if (strtotime($currentTime) > strtotime($time) + 1000)
-{
-//set online status to false
-$sql = "UPDATE leaderboard SET status = 'Offline' WHERE username = '" . $_SESSION['username'] . "'";
-$result = mysqli_query($conn, $sql);
-    if (!$result) {
-        echo "error updating online status";
-    }
-}
 ?>
